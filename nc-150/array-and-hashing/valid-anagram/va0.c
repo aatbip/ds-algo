8c47d9f8db60c0760860b5ef31500a6493f9a1d8
aatbip <anantabipal2010@gmail.com>
2026-02-05 19:04:33 +0545

    Add compFn and qsort

---
+++ nc-150/array-and-hashing/valid-anagram/va0.c
@@ -0,0 +1,37 @@
+/* Given two strings 's' and 't', return true if two strings are anagrams, otherwise return false.
+ *
+ * An anagram is a string that contains the exact same characters as another string, but the order
+ * of the characters can be different.
+ *
+ * In this program, we will detect if two strings are anagrams using the sort and compare technique.
+ * If two sorted strings are compared and found to be equal then they are anagram. The complexity
+ * of this method is:
+ * Time complexity - O(nlogn)
+ * Space complexity - O(1)
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compFn(const void *a, const void *b) {
+  char *s1 = (char *)a;
+  char *s2 = (char *)b;
+  return (*s1 > *s2) - (*s1 < *s2);
+}
+
+bool isAnagram(const char *s, const char *t) {
+  char *s1 = malloc(strlen(s) + 1);
+  char *s2 = malloc(strlen(t) + 1);
+  strcpy(s1, s);
+  strcpy(s2, t);
+  qsort(s1, strlen(s), sizeof(char), compFn);
+  qsort(s2, strlen(s), sizeof(char), compFn);
+
+  return false;
+}
+
+int main(void) {
+  isAnagram("zab", "hello");
+  return 0;
+}
