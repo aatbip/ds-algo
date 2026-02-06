4adfcbbef1b7a66c9b0a7573b041fbd3513aa13b
aatbip <anantabipal2010@gmail.com>
2026-02-06 10:14:36 +0545

    Add struct Pair in brute force approach for two sum

--- nc-150/array-and-hashing/two-sum/tw.c
+++ nc-150/array-and-hashing/two-sum/tw.c
@@ -1,5 +1,5 @@
 /* Given an array of integers `nums` and an integer `target`, return the indices i and j such that `nums[i] + nums[j] ==
- * target` and `i != j`. You may assume that every input has exactly one pair of indices i and j that satisfy the
+ * target` and `i != j`. You may assume that every input has exactly one Pair of indices i and j that satisfy the
  * condition. Return the answer with the smaller index first.
  *
  * In this program, we will solve two sum problem using brute force method.
@@ -10,25 +10,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void two_sum(int *nums, int target, int n, int *ans) {
-  ans[0] = ans[1] = -1;
+typedef struct {
+  int i, j;
+} Pair;
+
+Pair two_sum(int *nums, int target, int n) {
+  Pair x = {-1, -1};
   for (int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
       if (nums[i] + nums[j] == target) {
-        ans[0] = i;
-        ans[1] = j;
-        return;
+        x.i = i;
+        x.j = j;
+        return x;
       }
     }
   }
+  return x;
 }
 
 int main(void) {
   int arr[] = {1, 9, 2, 8, 4};
   int target = 11;
-  int ans[2];
-  two_sum(arr, target, 5, ans);
-  printf("%d ", ans[0]);
-  printf("%d \n", ans[1]);
+  Pair x = two_sum(arr, target, 5);
+  printf("%d %d\n", x.i, x.j);
   return 0;
 }
