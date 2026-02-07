7dc253c3823724b924ad4669b4d160f87fa1ac12
aatbip <anantabipal2010@gmail.com>
2026-02-07 14:52:21 +0545

    time complexity analysis

--- nc-150/array-and-hashing/top-k-frequent-elements/tkfe1.cc
+++ nc-150/array-and-hashing/top-k-frequent-elements/tkfe1.cc
@@ -10,6 +10,42 @@
  * We use min-heap instead of max-heap so that we can easily pop() the objects beyond 'k' because min-heap
  * arranges the objects with higher priority (in this case, objects having higher frequency count) as
  * children so that pop() will remove the objects with lower frequency count first.
+ *
+ * Time complexity -
+ * Let 'n' be the size of `nums`, and `k` be the requested top frequency count and 'u' be the unique
+ * elements in `nums`. Then,
+ *
+ *   for (auto num : nums) {
+ *     m[num]++;
+ *   }
+ *
+ * The above loop has O(n) time complexity.
+ *
+ *   for (auto el : m) {
+ *     pq.push({el.second, el.first});
+ *     if (pq.size() > k) {
+ *       pq.pop();
+ *     }
+ *   }
+ *
+ * Now in the above loop, the loop runs for 'u' times because there are 'u' unique elements and
+ * so the size of the hash map 'm' will be of 'u' length. Also, `push` and `pop` methods of the
+ * priority queue has time complexity of O(log k) because heap is not more than 'k'. So the
+ * time complexity of the above loop is: O(u logk) = O(n logk) in the worst case.
+ *
+    for (int i = 0; i < k; i++) {
+      res.push_back(pq.top().second);
+      pq.pop();
+    }
+ *
+ * Now finally the above loop have `pop` which is O(log k) and it runs for 'k' times so
+ * time complexity of this loop is O(k logk).
+ *
+ * Now the total time complexity is =
+ * O(n) + O(nlogn) + O(klogk)
+ * Worst case is dominated by O(nlogn).
+ * Therefore, time complexity is O(nlogn).
+ *
  * */
 
 #include <functional>
