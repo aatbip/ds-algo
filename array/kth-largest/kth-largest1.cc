1ebcdd9ea0824ec3589bacbe270b518ece3b8aa1
aatbip <anantabipal2010@gmail.com>
2026-02-09 14:13:11 +0545

    add partition function for quickselect

---
+++ array/kth-largest/kth-largest1.cc
@@ -0,0 +1,39 @@
+/*Given an integer array nums and an integer k, return the kth largest element in the array.
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ * Ex: Input: nums = [3,2,1,5,6,4], k = 2
+ *     Output: 5
+ *
+ * In this program, we will solve the problem using quickselect algorithm.
+ */
+
+#include <cstdlib>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int kth_largest(vector<int> &nums, int k) {
+    int left = 0, right = nums.size() - 1;
+    while (true) {
+      int pivot_index = rand() % (right - left + 1) + left;
+    }
+  }
+
+private:
+  int partition(vector<int> &nums, int left, int right, int pivot_index) {
+    int pivot = nums[pivot_index];
+    swap(nums[pivot_index], nums[right]);
+    int p = left;
+    for (int i = left; i < right; i++) {
+      if (nums[p] < pivot) {
+        swap(nums[i], nums[p]);
+        p++;
+      }
+    }
+    swap(nums[pivot_index], nums[right]);
+    return p;
+  }
+};
+
+int main(void) { return 0; }
