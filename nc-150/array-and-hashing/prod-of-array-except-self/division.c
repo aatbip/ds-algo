6ef7e191815b61859b8f7d5a8f1f888e8360d2c3
aatbip <anantabipal2010@gmail.com>
2026-02-11 13:45:17 +0545

    Solution based on product

---
+++ nc-150/array-and-hashing/prod-of-array-except-self/division.c
@@ -0,0 +1,50 @@
+/* Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except
+ * nums[i]. Each product is guaranteed to fit in a 32-bit integer.
+ * Example: Input: nums = [1,2,4,6]
+ *          Output: [48,24,12,8]
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *prod(int *arr, int n) {
+  int *p = malloc(sizeof(int) * n);
+  memset(p, 0, sizeof(*p));
+  int zero_count = 0;
+  int total_mul = 1;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] != 0) {
+      total_mul *= arr[i];
+    } else {
+      zero_count++;
+    }
+  }
+
+  if (zero_count > 1) {
+    return p;
+  }
+
+  if (zero_count == 1) {
+    for (int i = 0; i < n; i++) {
+      if (arr[i] == 0) {
+        p[i] = total_mul;
+        return p;
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    p[i] = total_mul / arr[i];
+  }
+  return p;
+}
+
+int main(void) {
+  int arr[] = {1, 0, 3, 2};
+  int *p = prod(arr, 4);
+  for (int i = 0; i < 4; i++) {
+    printf("%d ", p[i]);
+  }
+  free(p);
+  return 0;
+}
