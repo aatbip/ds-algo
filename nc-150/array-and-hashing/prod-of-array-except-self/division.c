0ac4a6e0342d9a59a5fb66eed6b8cd6a18b06eb0
aatbip <anantabipal2010@gmail.com>
2026-02-11 13:57:37 +0545

    Solution based on product made better

--- nc-150/array-and-hashing/prod-of-array-except-self/division.c
+++ nc-150/array-and-hashing/prod-of-array-except-self/division.c
@@ -11,10 +11,10 @@ int *prod(int *arr, int n) {
   int *p = malloc(sizeof(int) * n);
   memset(p, 0, sizeof(*p));
   int zero_count = 0;
-  int total_mul = 1;
+  int prod = 1;
   for (int i = 0; i < n; i++) {
     if (arr[i] != 0) {
-      total_mul *= arr[i];
+      prod *= arr[i];
     } else {
       zero_count++;
     }
@@ -24,23 +24,20 @@ int *prod(int *arr, int n) {
     return p;
   }
 
-  if (zero_count == 1) {
-    for (int i = 0; i < n; i++) {
-      if (arr[i] == 0) {
-        p[i] = total_mul;
-        return p;
-      }
-    }
-  }
-
   for (int i = 0; i < n; i++) {
-    p[i] = total_mul / arr[i];
+    if (zero_count > 0) {
+      if (arr[i] == 0) {
+        p[i] = prod;
+      }
+    } else {
+      p[i] = prod / arr[i];
+    }
   }
   return p;
 }
 
 int main(void) {
-  int arr[] = {1, 0, 3, 2};
+  int arr[] = {1, 2, 3, 2};
   int *p = prod(arr, 4);
   for (int i = 0; i < 4; i++) {
     printf("%d ", p[i]);
