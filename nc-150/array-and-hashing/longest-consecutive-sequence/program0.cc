c114a512c35775228ccb63407d6f70de253cd262
aatbip <anantabipal2010@gmail.com>
2026-02-15 10:20:08 +0545

    Working solution using hash set

--- nc-150/array-and-hashing/longest-consecutive-sequence/program0.cc
+++ nc-150/array-and-hashing/longest-consecutive-sequence/program0.cc
@@ -8,6 +8,7 @@
  *
  *
  *
+ *
  * */
 
 #include <iostream>
@@ -19,14 +20,24 @@ class Solution {
 public:
   int longest_conse_seq_len(vector<int> &nums) {
     unordered_set<int> num_set = {nums.begin(), nums.end()};
-
-    return 0;
+    int n = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (num_set.count(nums[i] - 1))
+        continue; // continue if lower number is present
+      int temp_n = 1;
+      while (num_set.count(++nums[i]))
+        temp_n++;
+      if (temp_n > n)
+        n = temp_n;
+    }
+    return n;
   }
 };
 
 int main(void) {
   Solution s;
-  vector<int> nums = {2, 20, 4, 10, 3, 5};
-  s.longest_conse_seq_len(nums);
+  // vector<int> nums = {2, 20, 4, 10, 3, 4, 5};
+  vector<int> nums = {0, 3, 2, 5, 4, 6, 1, 1};
+  cout << s.longest_conse_seq_len(nums) << "\n";
   return 0;
 }
