3916bd7b8a0178f4ff83f90deec212cd5a041166
aatbip <anantabipal2010@gmail.com>
2026-02-15 10:35:18 +0545

    Note on bruteforce

---
+++ nc-150/array-and-hashing/longest-consecutive-sequence/program1.cc
@@ -0,0 +1,12 @@
+/* Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be
+ * formed. A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous
+ * element. The elements do not have to be consecutive in the original array.
+ *
+ * Example: Input: int nums = {2,20,4,10,3,4,5}
+ *          Output: 4
+ *          Explanation: The longest consecutive sequence is {2, 3, 4, 5}.
+ *
+ * In this program we solve the problem using brute force method. Even though the brute force methods are generally
+ * not the optimal ones but it is a good starting point to get the intuition behind complex problems and a necessary
+ * way towards creating more optimal algorithms to solve the problem.
+ */
