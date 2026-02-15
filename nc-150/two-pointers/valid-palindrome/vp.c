a05227c96e32dd07342a6d8b956e7ca29c6235e8
aatbip <anantabipal2010@gmail.com>
2026-02-15 15:40:06 +0545

    Increment left and right pointers if not alnum character

---
+++ nc-150/two-pointers/valid-palindrome/vp.c
@@ -0,0 +1,31 @@
+/* Given a string s, return true if it is a palindrome, otherwise return false.
+ * A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all
+ * non-alphanumeric characters.
+ * Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+ * Example - Input: s = "Was it a car or a cat I saw?"
+ *           Output: true
+ *
+ * */
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool isPalindrome(char *str) {
+  int i = 0, j = strlen(str) - 1;
+  while (i < j) {
+    while (i < j && isalnum(str[i]) == 0)
+      i++;
+    while (i < j && isalnum(str[j]) == 0)
+      j--;
+  }
+  return true;
+}
+
+int main(void) {
+  char *str = "Was it a car or a cat I saw?";
+  printf("%d\n", isPalindrome(str));
+
+  return 0;
+}
