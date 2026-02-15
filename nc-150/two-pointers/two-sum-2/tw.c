ac6db4008a0707b2da313901d9d599144128f368
aatbip <anantabipal2010@gmail.com>
2026-02-15 18:16:36 +0545

    Implement two sum using binary search

--- nc-150/two-pointers/two-sum-2/tw.c
+++ nc-150/two-pointers/two-sum-2/tw.c
@@ -3,12 +3,13 @@
  * Note that index1 and index2 cannot be equal, therefore you may not use the same element twice.
  *
  * There will always be exactly one valid solution.
- *Example- Input: numbers = [1,2,3,4], target = 3
+ * Example- Input: numbers = [1,2,3,4], target = 3
  *         Output: [1,2]
  *
  * */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int binary_search(int *arr, int n, int key) {
   int low = 0, high = n - 1;
@@ -26,9 +27,23 @@ int binary_search(int *arr, int n, int key) {
   return -1;
 }
 
+int *two_sum(int *arr, int n, int target) {
+  int *res = malloc(sizeof(int) * 2);
+  for (int i = 0; i < n; i++) {
+    int temp = target - arr[i];
+    int k = binary_search(arr, n, temp);
+    if (k != -1) {
+      res[0] = k < i ? k + 1 : i + 1;
+      res[1] = k > i ? k + 1 : i + 1;
+      return res;
+    }
+  }
+  return NULL;
+}
+
 int main(void) {
   int arr[] = {2, 4, 5, 6, 8, 9, 11, 13};
-  printf("%d\n", binary_search(arr, 8, 133));
-
+  int *res = two_sum(arr, 8, 13);
+  printf("%d %d\n", res[0], res[1]);
   return 0;
 }
