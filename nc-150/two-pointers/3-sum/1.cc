25e5e79c3e5d885b956621600ee8dbfa6ae2ef5d
aatbip <anantabipal2010@gmail.com>
2026-02-16 10:41:54 +0545

    Loop through the sorted array to find triplets

--- nc-150/two-pointers/3-sum/1.cc
+++ nc-150/two-pointers/3-sum/1.cc
@@ -18,6 +18,15 @@ public:
   vector<vector<int>> three_sum(vector<int> &nums) {
     set<vector<int>> set;
     sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = i + 1; j < nums.size(); j++) {
+        for (int k = j + 1; k < nums.size(); k++) {
+          if (nums[i] + nums[j] + nums[k] == 0) {
+            set.insert({nums[i], nums[j], nums[k]});
+          }
+        }
+      }
+    }
   }
 };
 
