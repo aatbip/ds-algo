d7c8164bd62fff7349efcef965e2e69b0048c7e0
aatbip <anantabipal2010@gmail.com>
2026-02-16 14:18:12 +0545

    Create frequency hash map to count the occurence of each element

---
+++ nc-150/two-pointers/3-sum/2.cc
@@ -0,0 +1,33 @@
+/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where,
+ * nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+ * The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+ *
+ * Example- Input: vector<int> nums = {-1,0,1,2,-1,-4}
+ *          Output: {{-1,-1,2},{-1,0,1}}
+ *
+ */
+
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> three_sum(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    unordered_map<int, int> count;
+    for (int i : nums) {
+      count[i]++;
+    }
+    for (int i = 0; i < nums.size(); i++) {
+      count[nums[i]]--;
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      for (int j = 0; j < nums.size(); j++) {
+      }
+    }
+  }
+};
+
+int main(void) { return 0; }
