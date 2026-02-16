020bfd4b2a7d1a33d0fcfe96d6567fc036818a72
aatbip <anantabipal2010@gmail.com>
2026-02-16 10:41:16 +0545

    Declare set and sort the original nums

---
+++ nc-150/two-pointers/3-sum/1.cc
@@ -0,0 +1,29 @@
+/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where,
+ * nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+ * The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+ *
+ * Example- Input: vector<int> nums = {-1,0,1,2,-1,-4}
+ *          Output: {{-1,-1,2},{-1,0,1}}
+ * */
+
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> three_sum(vector<int> &nums) {
+    set<vector<int>> set;
+    sort(nums.begin(), nums.end());
+  }
+};
+
+int main(void) {
+  Solution s;
+  vector<int> nums = {-1, 0, 1, 2, -1, -4};
+  vector<vector<int>> res = s.three_sum(nums);
+  return 0;
+}
