8839732522651ee535404ac81c7e44deb00dc2bd
aatbip <anantabipal2010@gmail.com>
2026-02-17 14:55:29 +0545

    min function

---
+++ nc-150/two-pointers/container-with-most-water/1.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+int min(int a, int b) { return a < b ? a : b; }
+
+int water_content(int *heights, int n) {
+  int l = 0, r = n - 1;
+  while (l <= r) {
+  }
+}
+
+int main(void) {
+  int heights[] = {1, 7, 2, 5, 4, 7, 3, 6};
+
+  return 0;
+}
