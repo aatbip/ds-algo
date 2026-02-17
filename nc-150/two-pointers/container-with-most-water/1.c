1e40acb524f42da4f0825635fb1ee3866f8ee930
aatbip <anantabipal2010@gmail.com>
2026-02-17 15:00:36 +0545

    Add conditions to get max content

--- nc-150/two-pointers/container-with-most-water/1.c
+++ nc-150/two-pointers/container-with-most-water/1.c
@@ -4,8 +4,24 @@ int min(int a, int b) { return a < b ? a : b; }
 
 int water_content(int *heights, int n) {
   int l = 0, r = n - 1;
+  int am = 0;
   while (l <= r) {
+    if (l == r) {
+      am = heights[r] * 1;
+    }
+    if (heights[l] == 0) {
+      l++;
+    }
+    if (heights[r] == 0) {
+      r--;
+    }
+    int temp = min(heights[l], heights[r]) * (r - l);
+    if (temp > am)
+      am = temp;
+    l++;
+    r--;
   }
+  return am;
 }
 
 int main(void) {
