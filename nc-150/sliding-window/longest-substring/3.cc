dbb3322e72d0621ab994da0feacdb21c96af6dea
aatbip <anantabipal2010@gmail.com>
2026-02-19 18:10:41 +0545

    Note on optimal sliding window

--- nc-150/sliding-window/longest-substring/3.cc
+++ nc-150/sliding-window/longest-substring/3.cc
@@ -4,6 +4,9 @@
  *          Output: 3
  *          Explanation: The string "xyz" is the longest without duplicate characters.
  *
+ * In this program we solve the problem using sliding window but in a more optimal way then in 2.cc program.
+ * We make use of a hash map to store last index of the character so that we can move the 'l' pointer to
+ * the index beyond in one go without having to use a while loop internally.
  */
 
 #include <algorithm>
@@ -17,7 +20,7 @@ public:
   int longest_substring_len(string &s) {
     int res = 0;
     int l = 0;
-    unordered_map<char, int> map; // map to store frequency of each character
+    unordered_map<char, int> map; // map to store last index of each character
     for (int r = 0; r < s.size(); r++) {
       if (map.find(s[r]) != map.end()) {
         l = max(l, map[s[r]] + 1); // move l to next position
