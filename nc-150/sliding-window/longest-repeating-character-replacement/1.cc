054178aa26c25c51d3db98d22cc32c30a021d015
aatbip <anantabipal2010@gmail.com>
2026-02-25 14:40:01 +0545

    Add get_longest function in the Solution class

---
+++ nc-150/sliding-window/longest-repeating-character-replacement/1.cc
@@ -0,0 +1,28 @@
+/* You are given a string s consisting of only uppercase english characters and an integer k.
+ * You can choose up to k characters of the string and replace them with any other uppercase
+ * English character. After performing at most k replacements, return the length of the longest
+ * substring which contains only one distinct character.
+ * Example - Input: s = "XYYX", k = 2
+ *           Output: 4
+ *           Explanation: Either replace the 'X's with 'Y's, or replace the 'Y's with 'X's.
+ *
+ * */
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  int get_longest(string &s) {
+    int res;
+
+    return res;
+  }
+};
+
+int main(void) {
+  Solution s;
+  string str = "XYYX";
+  return 0;
+}
