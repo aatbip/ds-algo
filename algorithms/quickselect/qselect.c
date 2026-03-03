ad45c0fb1467e51587b71512d6575e550038b01d
aatbip <anantabipal2010@gmail.com>
2026-03-03 21:10:56 +0545

    Quick select intro

---
+++ algorithms/quickselect/qselect.c
@@ -0,0 +1,4 @@
+/* Quick select algorithm is used to select kth largest or kth smallest from the array. This algorithm is technically
+ * same as quick sort but instead of sorting completely we stop further sorting as soon as we find the index of kth
+ * largest or kth smallest.
+ * */
