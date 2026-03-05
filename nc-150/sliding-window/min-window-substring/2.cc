66f40da070ac4a1400f9a0abf6761e1aa739505b
aatbip <anantabipal2010@gmail.com>
2026-03-05 10:44:14 +0545

    Initialize countT and countS for frequency map

---
+++ nc-150/sliding-window/min-window-substring/2.cc
@@ -0,0 +1,26 @@
+/*Given two strings s and t, return the shortest substring of s such that every character in t,
+ * including duplicates, is present in the substring. If such a substring does not exist, return an empty string "".
+ * You may assume that the correct output is always unique.
+ *
+ * Example: Input: s = "OUZODYXAZV", t = "XYZ"
+ *          Output: "YXAZ"
+ */
+
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+  string minWindow(string s, string t) {
+    unordered_map<char, int> countT;
+    for (char c : t) {
+      countT[c]++;
+    }
+    int l = 0;
+    unordered_map<char, int> countS;
+    for (int r = 0; r < s.size(); r++) {
+      countS[s[r]]++;
+    }
+  }
+};
