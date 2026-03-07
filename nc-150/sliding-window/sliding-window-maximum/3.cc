ab8aba4af4353421dfd84126b21dd366bbf2ef69
aatbip <anantabipal2010@gmail.com>
2026-03-07 17:32:25 +0545

    Initialize deque, l and r

---
+++ nc-150/sliding-window/sliding-window-maximum/3.cc
@@ -0,0 +1,44 @@
+/*You are given an array of integers nums and an integer k. There is a sliding window of size k that
+ * starts at the left edge of the array. The window slides one position to the right until it reaches
+ * the right edge of the array.
+ * Return a list that contains the maximum element in the window at each step.
+ *
+ * Example: Input: nums = [1,2,1,0,4,2,6], k = 3
+ *          Output: [2,2,4,4,6]
+ *
+ *          Explanation:
+ *                      Window position            Max
+ *                      ---------------           -----
+ *                      [1  2  1] 0  4  2  6       2
+ *                      1 [2  1  0] 4  2  6        2
+ *                      1  2 [1  0  4] 2  6        4
+ *                      1  2  1 [0  4  2] 6        4
+ *                      1  2  1  0 [4  2  6]       6
+ *
+ */
+
+#include <deque>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> max_window(vector<int> &nums, int k) {
+    vector<int> res;
+    int l = 0, r = 0;
+    deque<int> dq;
+    return res;
+  }
+};
+
+int main(void) {
+  Solution s;
+  vector<int> nums = {1, 2, 1, 0, 4, 2, 6};
+  vector<int> res = s.max_window(nums, 3);
+  for (int el : res) {
+    cout << el << " ";
+  }
+  return 0;
+}
