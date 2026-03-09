7b430922a3ceb901042eadf2665687709eb4d5bf
aatbip <anantabipal2010@gmail.com>
2026-03-09 10:43:16 +0545

    Note and complexity analysis of hash map and stack based solution

--- nc-150/stack/valid-parenthesis/1.cc
+++ nc-150/stack/valid-parenthesis/1.cc
@@ -1,3 +1,16 @@
+/* You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+ * The input string s is valid if and only if:
+ * 1. Every open bracket is closed by the same type of close bracket.
+ * 2. Open brackets are closed in the correct order.
+ * 3. Every close bracket has a corresponding open bracket of the same type.
+ * Return `true` if s is a valid string, and false otherwise.
+ *
+ * This program solves the problem using stack and hash map.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(n)
+ * */
+
 #include <iostream>
 #include <stack>
 #include <string>
@@ -14,18 +27,18 @@ public:
       if (map.count(s[i])) {
         st.push(map[s[i]]);
       } else {
-        if (st.top() != s[i])
+        if (st.empty() || st.top() != s[i])
           return false;
         st.pop();
       }
     }
-    return true;
+    return st.empty();
   }
 };
 
 int main(void) {
   Solution s;
-  string input = "(]";
+  string input = "((";
   cout << s.is_valid_parenthesis(input) << "\n";
   return 0;
 }
