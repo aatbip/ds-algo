802fb3c0f2609bd55c6529632ebae54527668cda
aatbip <anantabipal2010@gmail.com>
2026-03-09 09:32:07 +0545

    Declare Solution class and calculate

---
+++ nc-150/stack/valid-parenthesis/1.cc
@@ -0,0 +1,18 @@
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  bool is_valid_parenthesis(string &s) {
+    stack<char> st;
+    int half = s.size() / 2;
+  }
+};
+
+int main(void) {
+  Solution s;
+
+  return 0;
+}
