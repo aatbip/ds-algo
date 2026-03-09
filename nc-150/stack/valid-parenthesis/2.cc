ba9a20cfa2e45c23d66d2ccf4a3ad7dc0c00a135
aatbip <anantabipal2010@gmail.com>
2026-03-09 10:48:39 +0545

    Solution without using hash map

---
+++ nc-150/stack/valid-parenthesis/2.cc
@@ -0,0 +1,44 @@
+/* You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+ * The input string s is valid if and only if:
+ * 1. Every open bracket is closed by the same type of close bracket.
+ * 2. Open brackets are closed in the correct order.
+ * 3. Every close bracket has a corresponding open bracket of the same type.
+ * Return `true` if s is a valid string, and false otherwise.
+ *
+ * This program solves the problem using only stack.
+ *
+ * */
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  int valid_parenthesis(string &s) {
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == '(') {
+        st.push(')');
+      } else if (s[i] == '{') {
+        st.push('}');
+      } else if (s[i] == '[') {
+        st.push(']');
+      } else {
+        if (st.empty() || st.top() != s[i]) {
+          return false;
+        }
+        st.pop();
+      }
+    }
+    return st.empty();
+  }
+};
+
+int main(void) {
+  Solution s;
+  string input = "((";
+  cout << s.valid_parenthesis(input) << "\n";
+  return 0;
+}
