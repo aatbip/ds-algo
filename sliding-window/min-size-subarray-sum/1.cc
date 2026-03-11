3fa99e434c5bd72f0e8d330644c53ea0c2645e94
aatbip <anantabipal2010@gmail.com>
2026-03-11 10:21:35 +0545

    Initialize left, res and initial sum

---
+++ sliding-window/min-size-subarray-sum/1.cc
@@ -0,0 +1,30 @@
+/*Given an array of positive integers nums and a positive integer target, return the minimal length
+ * of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+ *
+ * Example - Input: target = 7, nums = [2,3,1,2,4,3]
+ *           Output: 2
+ *           Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ ***/
+
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+  int min_subarray(std::vector<int> &nums, int target) {
+    int l = 0;
+    int res = INT_MAX;
+    int sum = 0;
+    for (int r = 0; r < nums.size(); r++) {
+    }
+  }
+};
+
+int main(void) {
+  Solution s;
+  std::vector<int> nums{2, 3, 1, 2, 4, 3};
+  std::cout << s.min_subarray(nums, 7) << "\n";
+  return 0;
+}
