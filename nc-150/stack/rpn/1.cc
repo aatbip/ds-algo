3ebf584d76f4ca04592fcab34deaf75709175c0d
aatbip <anantabipal2010@gmail.com>
2026-03-11 08:59:50 +0545

    Declare RPN class

---
+++ nc-150/stack/rpn/1.cc
@@ -0,0 +1,20 @@
+/* You are given an array of strings tokens that represents a valid arithmetic expression in
+ * Reverse Polish Notation (RPN).
+ * Return the integer that represents the evaluation of the expression.
+ * The operands may be integers or the results of other operations.
+ * The operators include '+', '-', '*', and '/'.
+ * Assume that division between integers always truncates toward zero.
+ * Example - Input: tokens = ["1","2","+","3","*","4","-"]
+ *           Output: 5
+ *           Explanation: ((1 + 2) * 3) - 4 = 5
+ * */
+
+class RPN {
+public:
+};
+
+int main(void) {
+  RPN s;
+
+  return 0;
+}
