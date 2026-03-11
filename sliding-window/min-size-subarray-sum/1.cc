b84c939a74216fa61660b98c7556ba2ad9a39e6e
aatbip <anantabipal2010@gmail.com>
2026-03-11 10:23:51 +0545

    Time and space complexity analysis

--- sliding-window/min-size-subarray-sum/1.cc
+++ sliding-window/min-size-subarray-sum/1.cc
@@ -4,6 +4,10 @@
  * Example - Input: target = 7, nums = [2,3,1,2,4,3]
  *           Output: 2
  *           Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ *
+ * Sliding window approach solves this problem.
+ * Time complexity - O(n)
+ * Space complexity - O(1)
  ***/
 
 #include <algorithm>
