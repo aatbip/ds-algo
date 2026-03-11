f0f9481a59da9416dcc0ae95ca32a6e350542d25
aatbip <anantabipal2010@gmail.com>
2026-03-11 10:21:59 +0545

    Check valid sliding window and get minimum res

--- sliding-window/min-size-subarray-sum/1.cc
+++ sliding-window/min-size-subarray-sum/1.cc
@@ -18,6 +18,11 @@ public:
     int res = INT_MAX;
     int sum = 0;
     for (int r = 0; r < nums.size(); r++) {
+      sum += nums[r];
+      while (sum >= target) {
+        res = std::min(res, r - l + 1);
+        sum -= nums[l++];
+      }
     }
   }
 };
