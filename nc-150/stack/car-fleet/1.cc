f83b4e82fe6e1ee9f09d6228337880a739e18ff4
aatbip <anantabipal2010@gmail.com>
2026-03-13 09:48:24 +0545

    Add target parameter

--- nc-150/stack/car-fleet/1.cc
+++ nc-150/stack/car-fleet/1.cc
@@ -3,16 +3,20 @@
 
 #include <algorithm>
 #include <iostream>
+#include <stack>
 #include <utility>
 #include <vector>
 class CarFleet {
 public:
-  int total_fleet(std::vector<int> &position, std::vector<int> &speed) {
+  int total_fleet(std::vector<int> &position, std::vector<int> &speed, int target) {
     std::vector<std::pair<int, int>> pair; // position,speed pair
     for (int i = 0; i < position.size(); i++) {
       pair.push_back({position[i], speed[i]});
     }
     std::sort(pair.rbegin(), pair.rend()); // sort in descending order wrt position
+    std::stack<int> stack;
+    for (auto &p : pair) {
+    }
   }
 };
 
@@ -20,6 +24,6 @@ int main(void) {
   CarFleet c;
   std::vector<int> position{4, 1, 0, 7};
   std::vector<int> speed{2, 2, 1, 1};
-  c.total_fleet(position, speed);
+  c.total_fleet(position, speed, 10);
   return 0;
 }
