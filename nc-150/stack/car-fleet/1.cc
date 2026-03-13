07d673681d50d4767289de81da50c04ab0f38ec3
aatbip <anantabipal2010@gmail.com>
2026-03-13 09:47:03 +0545

    Sort in descending order wrt position

--- nc-150/stack/car-fleet/1.cc
+++ nc-150/stack/car-fleet/1.cc
@@ -1,6 +1,8 @@
 /* Car fleet
  * */
 
+#include <algorithm>
+#include <iostream>
 #include <utility>
 #include <vector>
 class CarFleet {
@@ -8,9 +10,16 @@ public:
   int total_fleet(std::vector<int> &position, std::vector<int> &speed) {
     std::vector<std::pair<int, int>> pair; // position,speed pair
     for (int i = 0; i < position.size(); i++) {
-      pair[i] = {position[i], speed[i]};
+      pair.push_back({position[i], speed[i]});
     }
+    std::sort(pair.rbegin(), pair.rend()); // sort in descending order wrt position
   }
 };
 
-int main(void) { return 0; }
+int main(void) {
+  CarFleet c;
+  std::vector<int> position{4, 1, 0, 7};
+  std::vector<int> speed{2, 2, 1, 1};
+  c.total_fleet(position, speed);
+  return 0;
+}
