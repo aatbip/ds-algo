191ea02df920b3b2020331495b444e7fae24d6f5
aatbip <anantabipal2010@gmail.com>
2026-03-16 10:56:29 +0545

    Initialize required identifiers

---
+++ nc-150/stack/largest-rectangle-in-histogram/1.cc
@@ -0,0 +1,6 @@
+/*You are given an array of integers heights where heights[i] represents the height of a bar.
+ * The width of each bar is 1. Return the area of the largest rectangle that can be formed
+ * among the bars.
+ * Example- Input: heights = [7,1,7,2,2,4]
+ *          Output: 8
+ * */
