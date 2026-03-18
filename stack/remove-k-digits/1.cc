40e5ea5423b070cf042669aeeba0b6a7522f43de
aatbip <anantabipal2010@gmail.com>
2026-03-18 13:38:37 +0545

    Declare empty string as stack

---
+++ stack/remove-k-digits/1.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+  std::string smallInt(std::string num, int k) { std::string stack = ""; }
+};
+
+int main(void) {
+  Solution s;
+  // std::string num = "33526221184202197273";
+  std::string num = "1432219";
+  std::cout << s.smallInt(num, 3) << "\n";
+  return 0;
+}
