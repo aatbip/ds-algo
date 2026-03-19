52e16f55565e0b621b9f798c7ee49efeeca5a792
aatbip <anantabipal2010@gmail.com>
2026-03-19 12:47:38 +0545

    Declare stack

---
+++ stack/max-chunks-to-sorted/1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+class Solution {
+public:
+  int max_chunk(std::vector<int> &nums) { std::stack<int> stack; }
+};
+
+int main(void) {
+  Solution s;
+  std::vector<int> nums = {6, 5, 4, 3, 2};
+  std::cout << s.max_chunk(nums) << "\n";
+}
