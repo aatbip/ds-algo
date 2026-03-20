74e4736f87299512e4d503a800664cdedeae44ac
aatbip <anantabipal2010@gmail.com>
2026-03-20 11:32:33 +0545

    Initialize third to INT_MIN
    third is the 3rd element in 132 patter i.e. '2' element

---
+++ 1.cc
@@ -0,0 +1,17 @@
+#include <climits>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+class Solution {
+public:
+  bool find_123_pattern(std::vector<int> &nums) { std::cout << INT_MIN << "\n"; }
+};
+
+int main(void) {
+  Solution s;
+  std::vector<int> nums = {-1, 3, 2, 0};
+  std::cout << s.find_123_pattern(nums) << "\n";
+
+  return 0;
+}
