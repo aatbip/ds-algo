15546cdfabc417dd89e6c5ae82e5947d4b8e77ab
aatbip <anantabipal2010@gmail.com>
2026-03-20 11:42:31 +0545

    Time and space complexity

--- stack/132-pattern/1.cc
+++ stack/132-pattern/1.cc
@@ -9,6 +9,11 @@
  *           Input: nums = [-1,3,2,0]
  *           Output: true
  *           Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+ *
+ * Problem is solved using monotonic stack. `int third` is the kth element in the 132 pattern i.e. '2' element.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(n)
  * */
 
 #include <climits>
