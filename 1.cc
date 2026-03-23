e8648a25fc5c4b0c80d52080d0c339bc917ce2bc
aatbip <anantabipal2010@gmail.com>
2026-03-23 17:19:05 +0545

    Does not work for this case

--- 1.cc
+++ 1.cc
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include <stack>
 #include <vector>
 class Solution {
@@ -6,11 +7,27 @@ public:
   int find_unsorted_subarray(std::vector<int> &nums) {
     std::stack<int> stack;
     for (int i = 0; i < nums.size(); i++) {
-      while (!stack.empty() && stack.top() > nums[i]) {
-        stack.pop();
+      if (!stack.empty() && stack.top() > nums[i]) {
+        int top = stack.top();
+        while (!stack.empty() && stack.top() > nums[i]) {
+          stack.pop();
+        }
+        stack.push(top);
+      } else {
+        stack.push(nums[i]);
       }
-      stack.push(nums[i]);
     }
-    return stack.size();
+    return stack.size() == nums.size() ? 0 : stack.size();
   }
 };
+
+int main(void) {
+  Solution s;
+  std::vector<int> nums = {2, 1};
+  // std::vector<int> nums = {1, 2, 3, 4};
+  // std::vector<int> nums = {2, 6, 4, 8, 10, 9, 15};
+
+  std::cout << s.find_unsorted_subarray(nums) << "\n";
+
+  return 0;
+}
