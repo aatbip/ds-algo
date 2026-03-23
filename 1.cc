62fbf63de4d43a7b82d78242818bf4b6b94fd67d
aatbip <anantabipal2010@gmail.com>
2026-03-23 16:50:46 +0545

    Maintain monotonic stack

--- 1.cc
+++ 1.cc
@@ -6,8 +6,11 @@ public:
   int find_unsorted_subarray(std::vector<int> &nums) {
     std::stack<int> stack;
     for (int i = 0; i < nums.size(); i++) {
-
+      while (!stack.empty() && stack.top() > nums[i]) {
+        stack.pop();
+      }
       stack.push(nums[i]);
     }
+    return stack.size();
   }
 };
