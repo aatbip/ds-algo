ed432e1a7acb59e3d6265696f0ef7c05afde295c
aatbip <anantabipal2010@gmail.com>
2026-03-25 10:35:00 +0545

    Replace stack with string data type

--- stack/1.cc
+++ stack/1.cc
@@ -7,28 +7,22 @@ class Solution {
 public:
   std::string unique_smallest(std::string str) {
     std::vector<int> map(26, 0);
-    std::stack<char> stack;
+    std::string stack;
     std::vector<int> last(26);
     for (int i = 0; i < str.size(); i++) {
       last[str[i] - 'a'] = i;
     }
     for (int i = 0; i < str.size(); i++) {
       if (map[str[i] - 'a'] == 0) {
-        while (!stack.empty() && stack.top() > str[i] && last[stack.top() - 'a'] > i) {
-          map[stack.top() - 'a']--;
-          stack.pop();
+        while (!stack.empty() && stack.back() > str[i] && last[stack.back() - 'a'] > i) {
+          map[stack.back() - 'a']--;
+          stack.pop_back();
         }
-        stack.push(str[i]);
+        stack.push_back(str[i]);
         map[str[i] - 'a']++;
       }
     }
-    std::string s = "";
-    while (!stack.empty()) {
-      s += stack.top();
-      stack.pop();
-    }
-    std::reverse(s.begin(), s.end());
-    return s;
+    return stack;
   }
 };
 
