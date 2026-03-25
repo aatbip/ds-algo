b5df7fce808c8d8d1d463eb5f37a146b6e4cec7a
aatbip <anantabipal2010@gmail.com>
2026-03-25 09:49:47 +0545

    Define map and stack

---
+++ stack/1.cc
@@ -0,0 +1,11 @@
+
+#include <stack>
+#include <string>
+#include <unordered_map>
+class Solution {
+public:
+  std::string unique_smallest(std::string str) {
+    std::unordered_map<char, int> map;
+    std::stack<char> stack;
+  }
+};
