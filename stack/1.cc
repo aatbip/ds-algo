e9e6e6c374bfad27d756fdeba0dbabb35b8fdbd3
aatbip <anantabipal2010@gmail.com>
2026-03-25 09:51:29 +0545

    Decrement count in map and maintain monotonically increasing stack

--- stack/1.cc
+++ stack/1.cc
@@ -7,5 +7,13 @@ public:
   std::string unique_smallest(std::string str) {
     std::unordered_map<char, int> map;
     std::stack<char> stack;
+    for (char c : str) {
+      while (!stack.empty() && stack.top() > c) {
+        map[stack.top()]--;
+        stack.pop();
+      }
+
+      stack.push(c);
+    }
   }
 };
