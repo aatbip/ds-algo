6b7a9cf62b1a868ebfe7dad4bed5fa2e2114e9dc
aatbip <anantabipal2010@gmail.com>
2026-03-27 10:55:28 +0545

    correction: interchange row and col

--- nc-150/binary-search/search-2d-matrix/1.c
+++ nc-150/binary-search/search-2d-matrix/1.c
@@ -5,10 +5,10 @@
  *
  * Can you write a solution that runs in `O(log(m * n))` time?
  *
- * This solution solves the problem but in O(mlogn) time. It loops through the column to find the
- * required column then does a binary search on that column. So the iteration through the column
- * os size 'm' takes O(m) time and the binary search within the column takes O(logn) time and
- * binary search is performed only once for the required column. so,
+ * This solution solves the problem but in O(mlogn) time. It loops through the row to find the
+ * required row then does a binary search on that row. So the iteration through the total row
+ * 'm' takes O(m) time and the binary search within the row takes O(logn) time, size column size is 'n' and
+ * binary search is performed only once for the required row. so,
  * total O(m) + O(logn) = O(m+logn)
  *
  * It has O(1) space complexity.
@@ -36,12 +36,12 @@ int binary_search(int *arr, int n, int target) {
   return -1;
 }
 
-int search_2d_matrix(int (*matrix)[2], int col_size, int row_size, int target) {
-  for (int i = 0; i < col_size; i++) {
-    if (matrix[i][row_size - 1] < target) {
+int search_2d_matrix(int (*matrix)[4], int col_size, int row_size, int target) {
+  for (int i = 0; i < row_size; i++) {
+    if (matrix[i][col_size - 1] < target) {
       continue;
     } else {
-      int res = binary_search(matrix[i], row_size, target);
+      int res = binary_search(matrix[i], col_size, target);
       if (res == -1)
         return false;
       return true;
@@ -51,8 +51,7 @@ int search_2d_matrix(int (*matrix)[2], int col_size, int row_size, int target) {
 }
 
 int main(void) {
-  // int matrix[3][4] = {{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}};
-  int matrix[1][2] = {{1, 3}};
-  printf("%d\n", search_2d_matrix(matrix, 1, 2, 2));
+  int matrix[3][4] = {{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}};
+  printf("%d\n", search_2d_matrix(matrix, 4, 3, 13));
   return 0;
 }
