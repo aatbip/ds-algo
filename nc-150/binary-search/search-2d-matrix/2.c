8395297a7107f96fbfc19e40e27780445ec2dd40
aatbip <anantabipal2010@gmail.com>
2026-03-27 11:20:41 +0545

    Implement binary search by flattning the 2d matrix

---
+++ nc-150/binary-search/search-2d-matrix/2.c
@@ -0,0 +1,26 @@
+/*You are given an m x n 2-D integer array matrix and an integer target.
+ * -> Each row in matrix is sorted in non-decreasing order.
+ * -> The first integer of every row is greater than the last integer of the previous row.
+ * Return `true` if target exists within matrix or `false` otherwise.
+ *
+ * Can you write a solution that runs in `O(log(m * n))` time?
+ *
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+
+int binary_search_2d_matrix(int (*matrix)[4], int row_size, int col_size, int target) {
+  int low = 0, high = row_size * col_size - 1;
+  for (;;) {
+    if (low > high)
+      break;
+    int mid = (low + high) / 2;
+  }
+  return -1;
+}
+
+int main(void) {
+  int matrix[3][4] = {{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}};
+  printf("%d\n", binary_search_2d_matrix(matrix, 3, 4, 8));
+}
