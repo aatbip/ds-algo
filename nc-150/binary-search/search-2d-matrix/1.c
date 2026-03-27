e6b1834e9c490d8e5a489dd7c882685abc51c297
aatbip <anantabipal2010@gmail.com>
2026-03-27 10:31:56 +0545

    Define search_2d_matrix function

---
+++ nc-150/binary-search/search-2d-matrix/1.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int search_2d_matrix(int (*matrix)[2], int col_size, int row_size, int target) {
+  for (int i = 0; i < col_size; i++) {
+  }
+  return false;
+}
+
+int main(void) {
+  // int matrix[3][4] = {{1, 2, 4, 8}, {10, 11, 12, 13}, {14, 20, 30, 40}};
+  int matrix[1][2] = {{1, 3}};
+  printf("%d\n", search_2d_matrix(matrix, 1, 2, 2));
+  return 0;
+}
