bb0f44b1f7123a5cdc6290fd981c95e4dd043749
aatbip <anantabipal2010@gmail.com>
2026-03-28 21:00:21 +0545

    Explanation to the solution

--- nc-150/binary-search/koko-eating-bananas/2.cc
+++ nc-150/binary-search/koko-eating-bananas/2.cc
@@ -1,3 +1,27 @@
+/* You are given an integer array piles where piles[i] is the number of bananas
+ * in the ith pile. You are also given an integer h, which represents the number
+ * of hours you have to eat all the bananas.
+ * You may decide your bananas-per-hour eating rate of k. Each hour, you may choose
+ * a pile of bananas and eats k bananas from that pile. If the pile has less than k
+ * bananas, you may finish eating the pile but you can not eat from another pile in
+ * the same hour.
+ * Return the minimum integer k such that you can eat all the bananas
+ * within h hours.
+ *
+ * Example - Input: piles = [1,4,3,2], h = 9
+ *           Output: 2
+ *
+ * This program uses binary search approach to solve the problem.
+ * 'k' (eating speed) can range from 1 to 'm' where 'm' is the maximum element
+ * in the array of piles. So, we first initialize 'l' (low) as 1 and 'r' (high)
+ * as 'm'. Then we choose the mid (k) and then find the time taken for the mid (k).
+ * Next, if (t <= h) i.e. if time taken is less than 'h' then it is the valid result.
+ * We will update 'res' and then update r to k-1 (r=k-1) to check if there is another
+ * valid time from between 'l' to 'k-1'. Otherwise, we will update l to k+1 (l=k+1).
+ * This will help us find the result in O(logn) time using binary search.
+ *
+ */
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
