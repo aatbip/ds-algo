af7e43a5e0ba4066dd0ff2705224edbc862967bb
aatbip <anantabipal2010@gmail.com>
2026-03-28 20:51:10 +0545

    Initial setup and class definition

---
+++ nc-150/binary-search/koko-eating-bananas/2.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <vector>
+class Solution {
+public:
+  int min_eating_speed(std::vector<int> &piles, int h) {}
+};
+
+int main(void) {
+  Solution s;
+  std::vector<int> piles = {30, 11, 23, 4, 20};
+  std::cout << s.min_eating_speed(piles, 5) << "\n";
+  return 0;
+}
