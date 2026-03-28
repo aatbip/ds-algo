76bf7628504f2487d3df8de333f0fe614039311f
aatbip <anantabipal2010@gmail.com>
2026-03-28 18:38:33 +0545

    Problem setup and function definition

---
+++ nc-150/binary-search/koko-eating-bananas/1.c
@@ -0,0 +1,14 @@
+/*Brute force try*/
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+
+int min_eating_speed(int *piles, int n, int h) {}
+
+int main(void) {
+  int piles[] = {30, 11, 23, 4, 20};
+  int n = 5;
+  int h = 5; // max time in hour to finish all piles
+  printf("%d\n", min_eating_speed(piles, n, h));
+  return 0;
+}
