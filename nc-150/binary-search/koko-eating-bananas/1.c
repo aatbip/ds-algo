cfd1fe7e1a0d1780e6522caaae41ca083196ae19
aatbip <anantabipal2010@gmail.com>
2026-03-28 20:35:45 +0545

    Add question

--- nc-150/binary-search/koko-eating-bananas/1.c
+++ nc-150/binary-search/koko-eating-bananas/1.c
@@ -1,4 +1,18 @@
-/*Brute force try*/
+/* You are given an integer array piles where piles[i] is the number of bananas
+ * in the ith pile. You are also given an integer h, which represents the number
+ * of hours you have to eat all the bananas.
+ * You may decide your bananas-per-hour eating rate of k. Each hour, you may choose
+ * a pile of bananas and eats k bananas from that pile. If the pile has less than k
+ * bananas, you may finish eating the pile but you can not eat from another pile in
+ * the same hour.
+ * Return the minimum integer k such that you can eat all the bananas
+ * within h hours.
+ *
+ * Example - Input: piles = [1,4,3,2], h = 9
+ *           Output: 2
+ *
+ * */
+
 #include <limits.h>
 #include <math.h>
 #include <stdio.h>
