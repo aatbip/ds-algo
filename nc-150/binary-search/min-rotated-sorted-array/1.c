14aa8979f927c19efbb2055eac5f7d4a0ccca915
aatbip <anantabipal2010@gmail.com>
2026-03-29 11:03:45 +0545

    Initialize l and r and res

---
+++ nc-150/binary-search/min-rotated-sorted-array/1.c
@@ -0,0 +1,23 @@
+/*You are given an array of length n which was originally sorted in ascending order.
+ * It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+ * -> [3,4,5,6,1,2] if it was rotated 4 times.
+ * -> [1,2,3,4,5,6] if it was rotated 6 times.
+ *  Notice that rotating the array 4 times moves the last four elements of the array to the beginning.
+ *  Rotating the array 6 times produces the original array.
+ *  Assuming all elements in the rotated sorted array nums are unique, return the minimum element of this array.
+ *  A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+ * */
+
+int min_rotated_sorted(int *nums, int n) {
+  int l = 0, h = n - 1;
+  int res;
+  while (l < h) {
+    int mid = (l + h) / 2;
+  }
+}
+
+int main(void) {
+  int nums[] = {4, 5, 6, 7, 1, 2, 3};
+
+  return 0;
+}
