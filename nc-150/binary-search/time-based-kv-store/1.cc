aec7eadb397b3f5a33fb36f99c2fa582fad52e0b
aatbip <anantabipal2010@gmail.com>
2026-03-31 10:46:00 +0545

    Optimize binary search

--- nc-150/binary-search/time-based-kv-store/1.cc
+++ nc-150/binary-search/time-based-kv-store/1.cc
@@ -1,4 +1,5 @@
 
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <utility>
@@ -11,22 +12,28 @@ public:
   void set(std::string key, std::string value, int timeStamp) { map[key].push_back({value, timeStamp}); }
 
   std::string get(std::string key, int timeStamp) {
-    std::vector<std::pair<std::string, int>> vec = map.find(key)->second;
+    std::vector<std::pair<std::string, int>> vec = map[key];
+    std::string res = "";
     int l = 0, h = vec.size() - 1;
     while (l <= h) {
       int mid = (l + h) / 2;
-      if (vec[mid].second == timeStamp) {
-        return vec[mid].first;
-      }
       if (vec[mid].second > timeStamp) {
         h = mid - 1;
       }
-      if (vec[mid].second < timeStamp) {
+      if (vec[mid].second <= timeStamp) {
         l = mid + 1;
+        res = vec[mid].first;
       }
     }
-    return vec[h].first;
+    return res;
   }
 };
 
-int main(void) { return 0; }
+int main(void) {
+  TimeMap map;
+  map.set("hello", "world", 1);
+  map.set("dog", "milo", 2);
+
+  std::cout << map.get("dog", 4) << "\n";
+  return 0;
+}
