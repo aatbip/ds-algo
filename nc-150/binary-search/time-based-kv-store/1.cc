1de181201d086bc82f81f9cc5fcbdaf303546fbb
aatbip <anantabipal2010@gmail.com>
2026-03-31 10:25:53 +0545

    get method

--- nc-150/binary-search/time-based-kv-store/1.cc
+++ nc-150/binary-search/time-based-kv-store/1.cc
@@ -9,6 +9,24 @@ class TimeMap {
 
 public:
   void set(std::string key, std::string value, int timeStamp) { map[key].push_back({value, timeStamp}); }
+
+  std::string get(std::string key, int timeStamp) {
+    std::vector<std::pair<std::string, int>> vec = map.find(key)->second;
+    int l = 0, h = vec.size() - 1;
+    while (l <= h) {
+      int mid = (l + h) / 2;
+      if (vec[mid].second == timeStamp) {
+        return vec[mid].first;
+      }
+      if (vec[mid].second > timeStamp) {
+        h = mid - 1;
+      }
+      if (vec[mid].second < timeStamp) {
+        l = mid + 1;
+      }
+    }
+    return vec[h].first;
+  }
 };
 
 int main(void) { return 0; }
