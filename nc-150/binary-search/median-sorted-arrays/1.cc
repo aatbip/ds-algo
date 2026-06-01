85e99c578107f5655be809decdb82e478aa656f1
aatbip <anantabipal2010@gmail.com>
2026-06-01 17:14:42 +0545

    Preliminary setup

---
+++ nc-150/binary-search/median-sorted-arrays/1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  double medianSortedArrays(vector<int> &nums1, vector<int> &nums2) { return 0; }
+};
+
+int main(void) {
+  Solution s;
+  vector<int> nums1 = {1, 3, 8};
+  vector<int> nums2 = {7, 9, 10, 11};
+
+  cout << s.medianSortedArrays(nums1, nums2) << "\n";
+
+  return 0;
+}
