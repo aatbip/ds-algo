f574ec042493e8f8a2a1063978b1db3b8f9b1773
aatbip <anantabipal2010@gmail.com>
2026-06-01 17:26:27 +0545

    min and max calculation of partitioned array

--- nc-150/binary-search/median-sorted-arrays/1.cc
+++ nc-150/binary-search/median-sorted-arrays/1.cc
@@ -1,10 +1,23 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
-  double medianSortedArrays(vector<int> &nums1, vector<int> &nums2) { return 0; }
+  double medianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+    int l = 0, r = nums1.size();
+    while (1) {
+      int part1 = (l + r) / 2;
+      int part2 = (nums1.size() + nums2.size() + 1) / 2 - part1;
+      int max_left1 = max(nums1[0], nums1[part1 - 1]);
+      int max_left2 = max(nums2[0], nums2[part2 - 1]);
+      int min_right1 = min(nums1[part1], nums1[nums1.size() - 1]);
+      int min_right2 = min(nums2[part2], nums2[nums2.size() - 1]);
+    }
+
+    return 0;
+  }
 };
 
 int main(void) {
