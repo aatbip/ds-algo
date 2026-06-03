9826655c4fc1ef74f8c2843e9361bea95ef21ecd
aatbip <anantabipal2010@gmail.com>
2026-06-03 13:58:12 +0545

    Note on complexity

--- nc-150/linked-list/reverse-ll/2.cc
+++ nc-150/linked-list/reverse-ll/2.cc
@@ -1,3 +1,16 @@
+/*Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+ *
+ * Example 1-
+ *        Input : head = [0,1,2,3]
+ *        Output: [3,2,1,0]
+ * Example 1-
+ *        Input : head = []
+ *        Output: []
+ *
+ *  Time complexity- O(n)
+ *  Space complexity- O(1)
+ * */
+
 #include <iostream>
 
 class ListNode {
