1465d420bb0f908db3e40c0ff1cccfa189da3820
aatbip <anantabipal2010@gmail.com>
2026-06-04 16:08:32 +0545

    Prob setup

--- nc-150/linked-list/merge-two-sorted-lists/1.c
+++ nc-150/linked-list/merge-two-sorted-lists/1.c
@@ -1,7 +1,28 @@
-
+#include "../ll.h"
+#include <stdio.h>
 #include <stdlib.h>
 
+ll *merge_list(ll *list1, ll *list2) {
+  ll *prev = list1;
+  ll *next = list2;
+  while (next) {
+    if (next->key > prev->key) {
+      ll *temp = prev->next;
+    }
+  }
+}
+
 int main(void) {
-  struct ll *prev = NULL;
-  struct ll *cur;
+  ll *list1 = create_list(1);
+  insert_node(list1, 3);
+  insert_node(list1, 5);
+
+  ll *list2 = create_list(1);
+  insert_node(list1, 2);
+  insert_node(list1, 4);
+
+  download_ll(list1);
+  download_ll(list2);
+
+  return 0;
 }
