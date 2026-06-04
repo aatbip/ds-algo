d366872adfd2b2d23f62729705cb1688f69df37f
aatbip <anantabipal2010@gmail.com>
2026-06-04 18:32:27 +0545

    Dummy method

--- nc-150/linked-list/merge-two-sorted-lists/1.c
+++ nc-150/linked-list/merge-two-sorted-lists/1.c
@@ -3,48 +3,38 @@
 #include <stdlib.h>
 
 ll *merge_list(ll *list1, ll *list2) {
-  ll *prev = list1;
-  ll *next = list2;
-  ll *pprev = NULL;
-  if (!prev) {
-    return next;
-  } else if (!next) {
-    return prev;
-  }
-  while (next) {
-    if (next->key > prev->key || next->key == prev->key) {
-      ll *temp = prev->next;
-      pprev = prev;
-      prev->next = next;
-      prev = next;
-      next = temp;
+  ll *p1 = list1;
+  ll *p2 = list2;
+  ll dummy;
+  ll *tail = &dummy;
+  tail->next = NULL;
+  while (p1 && p2) {
+    if (p1->key <= p2->key) {
+      tail->next = p1;
+      p1 = p1->next;
     } else {
-      ll *temp = next->next;
-      next->next = prev;
-      if (pprev)
-        pprev->next = next;
-      next = temp;
+      tail->next = p2;
+      p2 = p2->next;
     }
+    tail = tail->next;
   }
-  if (list1->next)
-    return list1;
-  return list2;
+  return dummy.next;
 }
 
 int main(void) {
-  ll *list1 = create_list(2);
-  // insert_node(list1, 3);
-  // insert_node(list1, 5);
+  ll *list1 = create_list(1);
+  insert_node(list1, 3);
+  insert_node(list1, 5);
 
   ll *list2 = create_list(1);
-  // insert_node(list2, 2);
-  // insert_node(list2, 4);
+  insert_node(list2, 2);
+  insert_node(list2, 4);
 
   printf("before merge: \n");
   download_ll(list1);
   download_ll(list2);
 
-  printf("\nadter merge: \n");
+  printf("\nafter merge: \n");
   list1 = merge_list(list1, list2);
   download_ll(list1);
 
