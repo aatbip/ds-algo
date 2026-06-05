8ffb9c887d12774e00c4a8506e26bee4cd78a5c9
aatbip <anantabipal2010@gmail.com>
2026-06-05 19:02:57 +0545

    Note on  hash set based solution

--- nc-150/linked-list/linked-list-cycle/1.cc
+++ nc-150/linked-list/linked-list-cycle/1.cc
@@ -1,3 +1,18 @@
+/* Given the beginning of a linked list head, return true if there is a cycle in the linked list. Otherwise, return
+ * false. There is a cycle in a linked list if at least one node in the list can be visited again by following the next
+ * pointer. Internally, index determines the index of the beginning of the cycle, if it exists. The tail node of the
+ * list will set it's next pointer to the index-th node. If index = -1, then the tail node points to null and no cycle
+ * exists.
+ *
+ * Note: index is not given to you as a parameter.
+ *
+ * The above problem is solved using hash set in this program.
+ * Time complexity- O(n)
+ * Space complexity- O(n)
+ *
+ * We can write a better solution with respect to the space complexity.
+ * */
+
 #include <iostream>
 #include <unordered_set>
 
@@ -33,8 +48,8 @@ int main(void) {
   ListNode *temp = head = new ListNode(2, head);
   head = new ListNode(3, head);
 
-  // create a cycle
   ListNode *cur = head;
+  // create a cycle
   while (cur) {
     if (cur->next == nullptr) {
       cur->next = temp;
