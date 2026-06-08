46755135379517fcf1f6d42b144bef6f7dbc7b71
aatbip <anantabipal2010@gmail.com>
2026-06-08 16:28:01 +0545

    Two pointer + slow/fast pointer approach note

--- nc-150/linked-list/reorder-list/2.cc
+++ nc-150/linked-list/reorder-list/2.cc
@@ -1,3 +1,23 @@
+/*You are given the head of a singly linked-list.
+ * The positions of a linked list of length = 7 for example, can intially be represented as:
+ * [0, 1, 2, 3, 4, 5, 6]
+ *
+ * Reorder the nodes of the linked list to be in the following order:
+ * [0, 6, 1, 5, 2, 4, 3]
+ *
+ * Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
+ * [0, n-1, 1, n-2, 2, n-3, ...]
+ *
+ * You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
+ *
+ * Solution:
+ * This program uses slow and fast pointer approach to divide the linked list from the middle node. Then, the list from
+ * middle node to the tail is reversed and then two pointer approach is used to reorder the list.
+ *
+ * Time complexity- O(n)
+ * Space complexity- O(1), because no extra space is used
+ * */
+
 #include "../ll1.h"
 #include <cstring>
 #include <vector>
