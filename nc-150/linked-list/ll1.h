34220ba935deba49e17d9b116654150948fc230d
aatbip <anantabipal2010@gmail.com>
2026-06-08 16:30:13 +0545

    Clean obj file

--- nc-150/linked-list/ll1.h
+++ nc-150/linked-list/ll1.h
@@ -6,10 +6,10 @@
 #include <vector>
 
 class ListNode {
+public:
   int val;
   ListNode *next;
 
-public:
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *next) : val(x), next(next) {}
