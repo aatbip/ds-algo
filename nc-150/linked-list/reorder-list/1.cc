17fb2b3c4af108ed1b72026b7a0ee485d74d7ccd
aatbip <anantabipal2010@gmail.com>
2026-06-08 16:26:22 +0545

    Two pointer approach complexity and note

--- nc-150/linked-list/reorder-list/1.cc
+++ nc-150/linked-list/reorder-list/1.cc
@@ -1,3 +1,21 @@
+/*You are given the head of a singly linked-list.
+ * The positions of a linked list of length = 7 for example, can intially be represented as:
+ * [0, 1, 2, 3, 4, 5, 6]
+ *
+ * Reorder the nodes of the linked list to be in the following order:
+ * [0, 6, 1, 5, 2, 4, 3]
+ *
+ * Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
+ * [0, n-1, 1, n-2, 2, n-3, ...]
+ *
+ * You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
+ *
+ * Solution:
+ * This program uses two pointer method which adds a vector class to store the linked list's nodes.
+ * Time complexity- O(n)
+ * Space complexity- O(n), because of the vector
+ * */
+
 #include <cstring>
 #include <iostream>
 #include <vector>
