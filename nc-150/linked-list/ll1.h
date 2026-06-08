e128424e1d56521a74a1909139123b17c81a8318
aatbip <anantabipal2010@gmail.com>
2026-06-08 16:19:49 +0545

    Definition of ListNode class

---
+++ nc-150/linked-list/ll1.h
@@ -0,0 +1,34 @@
+#ifndef LL1_H
+#define LL1_H
+
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+class ListNode {
+  int val;
+  ListNode *next;
+
+public:
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+
+  static ListNode *make(std::vector<int> &arr) {
+    ListNode *head = nullptr;
+    for (int i = arr.size() - 1; i >= 0; --i) {
+      head = new ListNode(arr[i], head);
+    }
+    return head;
+  }
+
+  void print() {
+    ListNode *cur = this;
+    while (cur) {
+      std::cout << cur->val << "\n";
+      cur = cur->next;
+    }
+  }
+};
+
+#endif
