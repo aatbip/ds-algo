3f883e4f16a55b17f4d555496615f3e23a790429
aatbip <anantabipal2010@gmail.com>
2026-06-09 11:55:42 +0545

    Solution using brute force

--- nc-150/linked-list/remove-node-end/1.cc
+++ nc-150/linked-list/remove-node-end/1.cc
@@ -1,9 +1,43 @@
 #include "../ll1.h"
+#include <iostream>
 #include <vector>
 
+class Solution {
+public:
+  ListNode *remove_nth_node(ListNode *head, int n) {
+    ListNode *cur = head;
+    ListNode *prev = head;
+    int i = 0;
+    int size = 0;
+    while (cur) {
+      cur = cur->next;
+      size++;
+    }
+    cur = head;
+    while (cur) {
+      if (i == size - n) {
+        if (i == 0) {
+          return cur->next;
+        }
+        prev->next = cur->next;
+        break;
+      }
+      prev = cur;
+      cur = cur->next;
+      i++;
+    }
+    return head;
+  }
+};
+
 int main(void) {
-  std::vector<int> arr = {1, 2, 3, 4};
+  std::vector<int> arr = {1, 2};
   ListNode *head = ListNode::make(arr);
+  head->print();
+  std::cout << "\n";
+
+  Solution s;
+  head = s.remove_nth_node(head, 1);
   head->print();
 
   return 0;
