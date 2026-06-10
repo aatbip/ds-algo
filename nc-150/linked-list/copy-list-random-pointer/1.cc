ebe057dbf7b92c169400ba535bad8802fc9fe7a7
aatbip <anantabipal2010@gmail.com>
2026-06-10 11:06:23 +0545

    Create map of original to copy

--- nc-150/linked-list/copy-list-random-pointer/1.cc
+++ nc-150/linked-list/copy-list-random-pointer/1.cc
@@ -1,6 +1,18 @@
 #include "../ll1.h"
+#include <unordered_map>
 
 class Solution {
 public:
-  ListNode *deep_copy(ListNode *head) {}
+  ListNode *deep_copy(ListNode *head) {
+    ListNode *cur = head;
+    std::unordered_map<ListNode *, ListNode *> map;
+    while (cur) {
+      ListNode *copy = new ListNode(cur->val);
+      map[cur] = copy;
+      cur = cur->next;
+    }
+    cur = head;
+    while (cur) {
+    }
+  }
 };
