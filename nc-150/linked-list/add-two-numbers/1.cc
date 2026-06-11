d925dc817f4698fa220f06b39de8610e9042b23f
aatbip <anantabipal2010@gmail.com>
2026-06-11 11:24:11 +0545

    Solved using math and custom append method

--- nc-150/linked-list/add-two-numbers/1.cc
+++ nc-150/linked-list/add-two-numbers/1.cc
@@ -11,33 +11,31 @@ public:
       int sum = cur_l1->val + cur_l2->val + carry;
       if (sum >= 10) {
         carry = sum / 10;
-        s->append(sum - 10);
+        s = ListNode::append(s, sum - 10);
       } else {
-        s->append(sum);
+        s = ListNode::append(s, sum);
       }
-      cur_l1 = l1->next;
-      cur_l2 = l2->next;
+      cur_l1 = cur_l1->next;
+      cur_l2 = cur_l2->next;
     }
+    s = ListNode::append(s, carry);
     return s;
   }
 };
 
 int main(void) {
-  ListNode *l1 = nullptr;
-  l1->append(1);
-  l1->append(2);
-  // ListNode *l1 = new ListNode(9);
-  l1->append(1);
+  ListNode *l1 = new ListNode(9);
+  l1 = ListNode::append(l1, 1);
 
-  // ListNode *l2 = new ListNode(9);
-  // l2->append(9);
-  //
-  // l1->print();
-  // l2->print();
-  //
-  // Solution s;
-  // ListNode *sum = s.add_numbers(l1, l2);
-  // sum->print();
+  ListNode *l2 = new ListNode(9);
+  l2 = ListNode::append(l2, 9);
+
+  l1->print();
+  l2->print();
+
+  Solution s;
+  ListNode *sum = s.add_numbers(l1, l2);
+  sum->print();
 
   return 0;
 }
