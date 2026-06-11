b11728e89d4e0d5c57572dc3c5f484ec40a13aad
aatbip <anantabipal2010@gmail.com>
2026-06-11 13:17:48 +0545

    Fix for all cases

--- nc-150/linked-list/add-two-numbers/1.cc
+++ nc-150/linked-list/add-two-numbers/1.cc
@@ -3,35 +3,30 @@
 class Solution {
 public:
   ListNode *add_numbers(ListNode *l1, ListNode *l2) {
-    ListNode *s = nullptr;
+    ListNode *dummy = new ListNode(0);
+    ListNode *tail = dummy;
     int carry = 0;
     ListNode *cur_l1 = l1;
     ListNode *cur_l2 = l2;
-    while (cur_l1 && cur_l2) {
-      int sum = cur_l1->val + cur_l2->val + carry;
-      if (sum >= 10) {
-        carry = sum / 10;
-        s = ListNode::append(s, sum - 10);
-      } else {
-        s = ListNode::append(s, sum);
-      }
-      cur_l1 = cur_l1->next;
-      cur_l2 = cur_l2->next;
+    while (cur_l1 || cur_l2 || carry) {
+      int sum = (cur_l1 ? cur_l1->val : 0) + (cur_l2 ? cur_l2->val : 0) + carry;
+      carry = sum / 10;
+      tail->next = new ListNode(sum % 10);
+      tail = tail->next;
+      cur_l1 = cur_l1 ? cur_l1->next : nullptr;
+      cur_l2 = cur_l2 ? cur_l2->next : nullptr;
     }
-    s = ListNode::append(s, carry);
-    return s;
+    return dummy->next;
   }
 };
 
 int main(void) {
   ListNode *l1 = new ListNode(9);
+  l1 = ListNode::append(l1, 9);
   l1 = ListNode::append(l1, 1);
 
   ListNode *l2 = new ListNode(9);
   l2 = ListNode::append(l2, 9);
-
-  l1->print();
-  l2->print();
 
   Solution s;
   ListNode *sum = s.add_numbers(l1, l2);
