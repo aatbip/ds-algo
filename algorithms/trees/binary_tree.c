3a3f2f07a3cf656f90fc2be7afe1b170a350fe40
aatbip <anantabipal2010@gmail.com>
2026-06-12 13:56:19 +0545

    add btree_init

--- algorithms/trees/binary_tree.c
+++ algorithms/trees/binary_tree.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 
 typedef struct _Item {
   int key;
@@ -8,3 +9,17 @@ typedef struct node {
   struct node *l;
   struct node *r;
 } link;
+
+link *btree_init() {
+  link *node = malloc(sizeof(link));
+  node->item.key = 0;
+  node->l = NULL;
+  node->r = NULL;
+  return node;
+}
+
+int main(void) {
+  link *node = btree_init();
+
+  return 0;
+}
