fb23e9506d1679974f428b4e0afa5e6a26904e29
aatbip <anantabipal2010@gmail.com>
2026-06-16 10:04:55 +0545

    Create Node class and initializers

--- nc-150/linked-list/lru-cache/lru_cache.cc
+++ nc-150/linked-list/lru-cache/lru_cache.cc
@@ -1,11 +1,27 @@
+#include <unordered_map>
+
+class Node {
+  Node *next;
+  Node *prev;
+  int key;
+  int val;
+
+public:
+  Node(int k, int v) : key{k}, val{v}, next{nullptr}, prev{nullptr} {}
+};
+
 class LRUCache {
   int capacity;
   int size;
-  LRUCache *tail;
-  LRUCache *head;
+  std::unordered_map<int, LRUCache *> map;
+  Node *head;
 
 public:
-  LRUCache(int c) : capacity{c}, size{0}, head{nullptr}, tail{nullptr} {}
+  LRUCache(int c) {
+    capacity = c;
+    size = 0;
+    head = nullptr;
+  }
 };
 
 int main(void) {
