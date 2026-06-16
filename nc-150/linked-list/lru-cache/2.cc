003cac7462b0f4daa109e37faeac39f3f4524c01
aatbip <anantabipal2010@gmail.com>
2026-06-16 16:04:42 +0545

    Note

--- nc-150/linked-list/lru-cache/2.cc
+++ nc-150/linked-list/lru-cache/2.cc
@@ -1,3 +1,19 @@
+/*Implement the Least Recently Used (LRU) cache class LRUCache. The class should support the following operations:
+ * -> LRUCache(int capacity) Initialize the LRU cache of size capacity.
+ * -> int get(int key) Return the value corresponding to the key if the key exists, otherwise return -1.
+ * -> void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to
+ * the cache. If the introduction of the new pair causes the cache to exceed its capacity, remove the least recently
+ * used key.
+ *
+ * A key is considered used if a get or a put operation is called on it. Ensure that get and put each run in
+ * O(1) average time complexity.
+ *
+ * Solution: Used the standard template library's `list` data structure to realize the doubly linked list for LRU cache.
+ * Solved put and get in O(1) time complexity using the STL's `map` for O(1) lookup.
+ *
+ * Space complexity- O(n)
+ * */
+
 #include <iostream>
 #include <list>
 #include <unordered_map>
