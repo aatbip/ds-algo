a5853dce872ecfed1d7db3f171f6c1b33d49a6fd
aatbip <anantabipal2010@gmail.com>
2026-06-16 15:37:54 +0545

    Add put method

--- nc-150/linked-list/lru-cache/2.cc
+++ nc-150/linked-list/lru-cache/2.cc
@@ -5,9 +5,19 @@
 class LRUCache {
   int capacity;
   std::list<std::pair<int, int>> list;
-  std::unordered_map<int, std::list<int>::iterator> map;
+  std::unordered_map<int, std::list<std::pair<int, int>>::iterator> map;
 
   LRUCache(int capacity) : capacity{capacity} {}
 
-  void put(int key, int value) {}
+  void put(int key, int value) {
+    auto it = map.find(key);
+    if (it == map.end()) {
+      if (capacity == list.size()) {
+        map.erase(list.front().first);
+        list.pop_front();
+      }
+      list.push_back({key, value});
+      map[key] = --list.end();
+    }
+  }
 };
