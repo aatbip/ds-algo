447d0c645d2e825956884eea260fc6fb7bd9f52e
aatbip <anantabipal2010@gmail.com>
2026-06-16 10:26:32 +0545

    Unoptimized put and get

--- nc-150/linked-list/lru-cache/lru_cache.cc
+++ nc-150/linked-list/lru-cache/lru_cache.cc
@@ -1,26 +1,69 @@
 #include <unordered_map>
 
 class Node {
+public:
   Node *next;
   Node *prev;
   int key;
   int val;
 
-public:
   Node(int k, int v) : key{k}, val{v}, next{nullptr}, prev{nullptr} {}
 };
 
 class LRUCache {
   int capacity;
   int size;
-  std::unordered_map<int, LRUCache *> map;
+  std::unordered_map<int, Node *> map;
   Node *head;
+  Node *tail;
 
 public:
   LRUCache(int c) {
     capacity = c;
     size = 0;
     head = nullptr;
+    tail = nullptr;
+  }
+
+  void put(int k, int v) {
+    if (map.find(k) == map.end()) {
+      Node *node = new Node(k, v);
+      if (!this->head && !this->tail) { // ie. link is empty
+        this->head = node;
+        node->next = nullptr;
+        this->tail = node;
+        node->prev = nullptr;
+      } else {
+        if (size == capacity) {
+          Node *temp = this->head;
+          this->head = this->head->next;
+          delete (temp);
+          this->size--;
+        }
+        node->prev = tail;
+        this->tail->next = node;
+        node->next = nullptr;
+        this->tail = node;
+      }
+      this->size++;
+    } else {
+      Node *p = map.find(k)->second;
+      p->val = v;
+      this->tail->next = p;
+      p->prev = this->tail;
+      this->tail = p;
+    }
+  }
+
+  int get(int k) {
+    if (map.find(k) == map.end())
+      return -1;
+    Node *p = map.find(k)->second;
+    this->tail->next = p;
+    p->prev = this->tail;
+    this->tail = p;
+    int val = p->val;
+    return val;
   }
 };
 
