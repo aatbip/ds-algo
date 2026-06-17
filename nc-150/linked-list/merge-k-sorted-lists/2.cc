30d677658f61bf409c0772ff860cdd9a137fec6d
aatbip <anantabipal2010@gmail.com>
2026-06-17 17:11:18 +0545

    Divide and conquer method

--- nc-150/linked-list/merge-k-sorted-lists/2.cc
+++ nc-150/linked-list/merge-k-sorted-lists/2.cc
@@ -8,13 +8,44 @@
 #include "../ll1.h"
 
 class Solution {
+  ListNode *merge_two_list(ListNode *l1, ListNode *l2) {
+    ListNode *cur1 = l1;
+    ListNode *cur2 = l2;
+    ListNode *dummy = new ListNode(0);
+    ListNode *tail = dummy;
+    while (cur1 && cur2) {
+      if (cur1->val <= cur2->val) {
+        tail->next = cur1;
+        cur1 = cur1->next;
+      } else {
+        tail->next = cur2;
+        cur2 = cur2->next;
+      }
+      tail = tail->next;
+    }
+    tail->next = cur1 ? cur1 : cur2;
+    return dummy->next;
+  }
 
 public:
-  ListNode *mergeKLists(std::vector<ListNode *> &lists) {}
+  ListNode *mergeKLists(std::vector<ListNode *> &lists) {
+    if (lists.empty())
+      return nullptr;
+    while (lists.size() > 1) {
+      std::vector<ListNode *> m;
+      for (int i = 0; i < lists.size() - 1; i = i + 2) {
+        m.push_back(merge_two_list(lists[i], lists[i + 1]));
+      }
+      if (lists.size() % 2 > 0) {
+        m.push_back(lists[lists.size() - 1]);
+      }
+      lists = m;
+    }
+    return lists[0];
+  }
 };
 
 int main() {
-
   std::vector a = {1, 2, 5, 10, 12, 44};
   std::vector b = {2, 4, 8};
   std::vector c = {10, 19, 21};
@@ -26,6 +57,7 @@ int main() {
 
   Solution s;
   ListNode *m = s.mergeKLists(list);
+  m->print();
 
   return 0;
 }
