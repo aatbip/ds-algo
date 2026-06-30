2e731e88227a852bbf95f8129abc1b65f6c940d0
aatbip <anantabipal2010@gmail.com>
2026-06-30 19:16:59 +0545

    Add init operation that creates a dummy root node and initializes bst_ctx

--- algorithms/trees/bst.c
+++ algorithms/trees/bst.c
@@ -7,8 +7,20 @@ typedef struct _bst_node {
 } bst_node_t;
 
 typedef struct _bst_ctx {
-  bst_node_t root; // root node
-  int count;       // number of nodes
+  bst_node_t *root; // root node
+  int count;        // number of nodes
 } bst_ctx_t;
 
 static bst_ctx_t bst_ctx;
+
+/*init() create a dummy root*/
+bst_node_t *init() {
+  bst_node_t *root = malloc(sizeof(bst_node_t));
+  root = NULL;
+
+  // initialize bst_ctx
+  bst_ctx.count = 0;
+  bst_ctx.root = root;
+
+  return root;
+}
