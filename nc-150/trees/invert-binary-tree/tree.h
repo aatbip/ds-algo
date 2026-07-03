3adf8f0743b2387d33f31af9d9b7fa1b3bbb6004
aatbip <anantabipal2010@gmail.com>
2026-07-03 09:15:20 +0545

    Add bfs level order traverse helper in TreeNode class

--- nc-150/trees/invert-binary-tree/tree.h
+++ nc-150/trees/invert-binary-tree/tree.h
@@ -1,6 +1,8 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <iostream>
+#include <queue>
 class TreeNode {
 public:
   int val;
@@ -9,6 +11,18 @@ public:
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+
+  void bfs_levelorder(TreeNode *root) {
+    std::queue<TreeNode *> queue;
+    queue.push(root);
+    while (!queue.empty()) {
+      TreeNode *node = queue.front();
+      std::cout << node->val << "\n";
+      queue.pop();
+      queue.push(root->left);
+      queue.push(root->right);
+    }
+  }
 };
 
 #endif
