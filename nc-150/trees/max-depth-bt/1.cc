3d82b8b17ef1ec1b015fb65c8b0c8b5f1cf7ff0c
aatbip <anantabipal2010@gmail.com>
2026-07-04 13:08:52 +0545

    Note on recursive solution's space complexity

--- nc-150/trees/max-depth-bt/1.cc
+++ nc-150/trees/max-depth-bt/1.cc
@@ -1,3 +1,15 @@
+/*Given the root of a binary tree, return its depth. The depth of a binary tree is defined as the number of nodes
+ * along the longest path from the root node down to the farthest leaf node.
+ *
+ * Solution:
+ * Solved using recursion.
+ * Time complexity- o(n)
+ * Space complexity- O(h), where h = log(n) in best case (if tree is balanced) while h = n in worst case (if tree is
+ * unbalanced). This is because, when max_depth is resolved, the stack frame gets created and gets unwinded when
+ * max_depth returns. If the tree is balanced then the left subtree and right subtree of every node is balanced making
+ * the stack frame to unwind sooner using only log(n) space.
+ * */
+
 #include "../tree.h"
 #include <algorithm>
 #include <iostream>
