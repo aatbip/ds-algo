1f0e734dcdca5c541455bd03853852ee07122545
aatbip <anantabipal2010@gmail.com>
2026-07-06 11:14:51 +0545

    Note on solution

--- nc-150/trees/diameter-bt/1.cc
+++ nc-150/trees/diameter-bt/1.cc
@@ -1,3 +1,17 @@
+/*The diameter of a binary tree is defined as the length of the longest path between any two nodes within the tree.
+ * The path does not necessarily have to pass through the root. The length of a path between two nodes in a binary tree
+ * is the number of edges between the nodes. Note that the path can not include the same node twice.
+ *
+ * Given the root of a binary tree root, return the diameter of the tree.
+ *
+ * Solution-
+ * Measure the height of left subtree and right subtree for each node. The sum of the height of left subtree and right
+ * subtree for each node is the diameter through that node. Calculate this for every node and return the highest
+ * diameter.
+ *
+ * Time complexity: O(n)
+ * Space complexity: O(h), where h=log(n) if tree is balanced otherwise h=n for worst case unbalanced tree
+ * */
 
 #include "../tree.h"
 #include <algorithm>
