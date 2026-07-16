dad54ef1d7a32dd0f7909c03221ec8b5115ca697
aatbip <anantabipal2010@gmail.com>
2026-07-16 13:04:57 +0545

    Recursive DFS

--- nc-150/trees/validate-bst/1.cc
+++ nc-150/trees/validate-bst/1.cc
@@ -1,16 +1,16 @@
 #include "../tree.h"
+#include <climits>
 
 class Solution {
 private:
-  bool valid_bst(TreeNode *root, TreeNode *left, TreeNode *right) {
-    if (!root)
+  bool valid_bst(TreeNode *node, long leftVal, long rightVal) {
+    if (!node)
       return true;
-    if (!(left->val < root->val && right->val > root->val))
+    if (!(leftVal < node->val && rightVal > node->val))
       return false;
-    return valid_bst(root->left, root->left->left, root->left->right) &&
-           valid_bst(root->right, root->right->left, root->right->right);
+    return valid_bst(node->left, leftVal, node->val) && valid_bst(node->right, node->val, rightVal);
   }
 
 public:
-  bool isValidBST(TreeNode *root) { return valid_bst(root, root->left, root->right); }
+  bool isValidBST(TreeNode *root) { return valid_bst(root, LONG_MIN, LONG_MAX); }
 };
