bd31151b22c5bb99765a069e598ec061fd018901
aatbip <anantabipal2010@gmail.com>
2026-09-22 10:23:49 +0545

    construct bt

--- nc-150/trees/construct-bt/1.cc
+++ nc-150/trees/construct-bt/1.cc
@@ -6,10 +6,21 @@ class Solution {
   int idx = 0;
   std::unordered_map<int, int> map;
 
+  TreeNode *bt(std::vector<int> &preorder, int l, int r) {
+    if (l > r)
+      return nullptr;
+    int mid = map[preorder[idx]];
+    TreeNode *root = new TreeNode(preorder[idx++]);
+    root->left = bt(preorder, l, mid - 1);
+    root->right = bt(preorder, mid + 1, r);
+    return root;
+  }
+
 public:
   TreeNode *const_bt(std::vector<int> &preorder, std::vector<int> &inorder) {
     for (int i = 0; i < inorder.size(); i++) {
       map[inorder[i]] = i;
     }
+    return bt(preorder, 0, inorder.size() - 1);
   }
 };
