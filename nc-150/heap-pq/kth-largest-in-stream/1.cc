ea4329ad050cf736b4c082d637886c27ea18258d
aatbip <anantabipal2010@gmail.com>
2026-09-24 13:59:30 +0545

    Better off solution

--- nc-150/heap-pq/kth-largest-in-stream/1.cc
+++ nc-150/heap-pq/kth-largest-in-stream/1.cc
@@ -2,22 +2,25 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+
 class KthLargest {
   int k;
   std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
 
 public:
   KthLargest(int k, std::vector<int> &nums) {
-    //
     this->k = k;
     for (int &n : nums) {
       min_heap.push(n);
+      if (min_heap.size() > k) {
+        min_heap.pop();
+      }
     }
   }
 
   int add(int val) {
     min_heap.push(val);
-    for (int i = 0; i <= min_heap.size() - k; i++) {
+    if (min_heap.size() > k) {
       min_heap.pop();
     }
     return min_heap.top();
